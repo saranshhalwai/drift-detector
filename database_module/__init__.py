2dd42947d76ac17680627623061cee0c4565000d
Harsh Bhati <harshbhati814@gmail.com>
2025-06-09 18:30:23 +0530

    integrated db.py with server.py

--- database_module/__init__.py
+++ database_module/__init__.py
@@ -1,2 +1,3 @@
+#database_module/__init__.py
 from .db import init_db
 from .mcp_tools import get_all_models_handler, search_models_handler
--- database_module/db.py
+++ database_module/db.py
@@ -1,3 +1,4 @@
+#database_module/db.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
